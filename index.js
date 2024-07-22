b22862de00671316cf3b6ab806542d04c2f0816e
amina134 <aminakouni14@gmail.com>
2024-07-22 14:47:16 +0100

    first commit

--- index.js
+++ index.js
@@ -1,145 +1,84 @@
-let ch = "  . haith gauda 6 hhgdz  ." ;
-
-function rev(ch){
-    let ch2 ="";
-
-    let i=ch.length-1
-    for ( i ; i>=0 ; i-- ) {
-        ch2+=ch[i];
+var btnplus = document.getElementsByClassName('fa-plus-circle');
+console.log(btnplus);
+var totalElement = document.getElementsByClassName('total')[0]; // Access the first element of total
+var price = document.getElementsByClassName('unit-price');
+console.log(price[0].textContent);
+var quan = document.getElementsByClassName('quantity');
+for (let i = 0; i < btnplus.length; i++) {
+     btnplus[i].addEventListener('click', function add() {
         
-    }
+         quan[i].textContent = parseInt(quan[i].textContent) + 1;
+ 
+        
+         let sum = 0; 
+         for (let j = 0; j < btnplus.length; j++) {
+             let itemTotal = parseInt(price[j].textContent) * parseInt(quan[j].textContent);
+             sum += itemTotal;
+         }
+ 
+        
+         totalElement.textContent = sum;
+     });
+ }
+var sum1=0;
 
-    return ch2 ;
+var btnmoins=document.getElementsByClassName('fas fa-minus-circle')
+console.log(btnmoins[0]);
+for (let i = 0; i < btnmoins.length; i++) {
+     btnmoins[i].addEventListener('click', function sub() {
+         if (parseInt(quan[i].textContent) > 0) {
+            
+             quan[i].textContent = parseInt(quan[i].textContent) - 1;
+             
+             
+             let sum = 0; 
+             for (let j = 0; j < price.length; j++) {
+                 let itemTotal = parseInt(price[j].textContent) * parseInt(quan[j].textContent);
+                 sum += itemTotal; 
+             }
+             
+             
+             totalElement.textContent = sum;
+         }
+     });
+ }
+
+//do the quatity 
+var quan=document.getElementsByClassName('quantity')
+var delet=document.getElementsByClassName(' fa-trash-alt')
+for (let i = 0; i < delet.length; i++) {
+     var button=delet[i];
+     button.addEventListener('click',function(event){
+          var buttonclicked=event.target
+          buttonclicked.parentElement.parentElement.parentElement.remove()
+          let sum = 0; 
+          for (let j = 0; j < price.length; j++) {
+              let itemTotal = parseInt(price[j].textContent) * parseInt(quan[j].textContent);
+              sum += itemTotal; 
+          }
+          
+          
+          totalElement.textContent = sum;
+
+     })
+     
 }
 
+var like = document.getElementsByClassName('fa-heart');
 
-function len (ch)
-{
-    return ch.length;
-}
+for (let i = 0; i < like.length; i++) {
+    let likeit = like[i];
 
-
-function cap(ch)
-{
-    let ch2=ch[0].toUpperCase();
     
-    for (let i=1; i<ch.length ;i++)
-        {
-    
-           if(ch.charAt(i)!=" ")
-           {
-            ch2+=ch[i];
+    likeit.addEventListener('click', function() {
+      
+        var currentColor = window.getComputedStyle(likeit).color;
 
-           }
-           else
-           {
-            ch2+=" ";
-            ch2+=ch[i+1].toUpperCase();
-            i++;
-
-           }
-           
-
-
+        
+        if (currentColor === 'rgb(255, 0, 0)') { 
+            likeit.style.color = 'grey';
+        } else {
+            likeit.style.color = 'red'; 
         }
-
-
-   
-    return ch2 ;
+    });
 }
-
-var arr=[1,2,3,4,5,6];
-
-function minmax (arr)
-{
-    return [Math.min(...arr), Math.max(...arr)] ;
-}
-
-
-
-function sum(arr)
-{
-    let sum =0 ;
-    for( let i of arr)
-    {
-        sum+=i;
-    }
-    return sum ;
-}
-
-function trieCondition(arr,condition){
-    let trieArray=[];
-    for(const element of arr)
-        {
-        if(condition(element))
-            {
-            trieArray.push(element);
-            }
-        }
-    return trieArray ;
-
-}
-
-
-var n=5;
-function fact(n){
-     let  p=1;
-    for(let i=1;i<=n;i++){
-       p=p*i;
-    }
-    return p;
-}
-
-
-
-let x=1;
-function prem (x)
-{
-    if (x==1)
-    {
-        return false ;
-    }
-
-    for (let i=2; i<x/2; i++)
-    {
-        if(x%i==0 )
-        {
-         
-         return  false;
-        }
-
-    }
-    return true;
-
-}
-
-
-let u0=0;
-let u1=1;
-
-
-y=17;
-function fub(y)
-{
-    let resf=[0,1]
-    for (i=2;i<y;i++)
-    {
-        resf.push(resf[i-2]+resf[i-1])
-    }
-
-    return resf ;
-
-}
-
-
-
-
-
-
-
-
-
-
-console.log(fub(y));
-
-
